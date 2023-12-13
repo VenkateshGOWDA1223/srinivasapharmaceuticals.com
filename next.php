d1946a04013037a99566e5d2bfe2b6a9b2212b6f
VenkateshGOWDA1223 <153351455+VenkateshGOWDA1223@users.noreply.github.com>
2023-12-13 19:35:55 +0530

    Delete next.php

--- next.php
+++
@@ -1,5 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Validate username and password (this is a simplified example)
-    $username = $_POST["username"];
-$password = $_POST["password"];
